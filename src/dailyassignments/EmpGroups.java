bb66d56f5192f1f62685fb1491296d8f082ba706
vinodkondapalli <vinodnani304@gmail.com>
2025-11-05 14:33:28 +0530

    Assignment 3 - parent & ChildClass

---
+++ src/dailyassignments/EmpGroups.java
@@ -0,0 +1,20 @@
+package dailyassignments;
+
+public class EmpGroups extends Employee {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	Employee emp=new Employee();
+	String [] empgroup= {emp.emp1,emp.emp2,emp.emp3};
+	int [] empgroupid= {emp.emp1id,emp.emp2id,emp.emp3id};
+	System.out.println("Employee1 Name : " + empgroup[0] + " , Employe1 Id : " +  empgroupid[0]);
+	System.out.println("Employee1 Name : " + empgroup[1] + " , Employe1 Id : " +  empgroupid[1]);
+	System.out.println("Employee1 Name : " + empgroup[2] + " , Employe1 Id : " +  empgroupid[2]);
+
+	
+	}
+	
+
+	}
+
+
---
+++ src/dailyassignments/Employee.java
@@ -0,0 +1,21 @@
+package dailyassignments;
+
+public class Employee {
+
+	// Employee names
+	String emp1 = "Bharath";
+	String emp2 = "John";
+	String emp3 = "Aarav";
+
+	// Employee IDs
+	int emp1id = 1234;
+	int emp2id = 1235;
+	int emp3id = 1236;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+    ;
+	}
+
+}
